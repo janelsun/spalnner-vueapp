aea0274f5d92f79091a7510968416232ab6e1a00
janelsun <sun.janel.yichen@gmail.com>
2019-02-22 10:43:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img width="25%" src="./assets/logo.png"> -->
+    <PersonalInfo /> <AcademicProgress />
+  </div>
+</template>
+
+<script>
+import PersonalInfo from "./components/PersonalInfo.vue";
+import AcademicProgress from "./components/AcademicProgress.vue";
+
+export default {
+  name: "App",
+  components: {
+    PersonalInfo,
+    AcademicProgress
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
